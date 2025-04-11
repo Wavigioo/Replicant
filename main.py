9be97b7aca5c2d890d2624d23d3988a7062d2cbc
whycantibelinus <157988594+whycantibelinus@users.noreply.github.com>
2025-04-11 06:08:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 from core.compressor import basic_compress
+from core.log import log_compression
+from core.analyze import analyze_compression
+
 def run():
     print("[Replicant] Reading input file...")
 
-  # Step 1: Open and read the input file
-    with open("input/sample_date.txt", "r") as infile:
-        raw_data = infile.read()
+    with open("input/sample_data.txt", "r") as infile: raw_data = infile.read()
 
     print("[Replicant] Compressing data...")
     compressed_data = basic_compress(raw_data)
 
-  # Step 2: Write the compressed result to a new output file
+    # Analyze and Log
+    efficiency = analyze_compression(raw_data, compressed_data)
+    log_compression(raw_data, compressed_data)
+
+    print(f"[Replicant] Compression efficiency: {efficiency}%")
+
+
     with open("output/compressed_output.txt", "w") as outfile:
         outfile.write(compressed_data)
 
-  print("[Replicant] Compression complete. Output saved to output/compressed_output.txt.")
+    print("[Replicant] compression complete. Output saved to output/compressed_output.txt.")
 
 if __name__ == "__main__":
     run()
