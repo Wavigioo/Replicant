a992940b6757f1a9cb678e193a618d518bd8437b
whycantibelinus <157988594+whycantibelinus@users.noreply.github.com>
2025-04-11 06:20:46 -0600

    Update log.py

--- core/log.py
+++ core/log.py
@@ -2,5 +2,7 @@ import time
 
 def log_compression(input_text, output_text, method="basic_compress", efficiency=None):
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
-    log_entry = (f"[{timestamp}] Method: {method} I " f"Input length: {len(input_text)} I Output length: {len(output_text)}\n")
+    log_entry = (f"[{timestamp}] Method: {method} I "
+                 f"Input length: {len(input_text)} I Output length: {len(output_text)} I "
+                 f"Efficiency: {efficiency}%\n")
     with open("logs/replicant.log", "a") as logfile: logfile.write(log_entry)
